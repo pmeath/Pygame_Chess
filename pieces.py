e46a67223175f4701ad9e193d96d5ab141798694
Patrick <pmeath@gmail.com>
2020-07-08 00:49:13 +0100

    Added click function.

--- pieces.py
+++ pieces.py
@@ -1,82 +1,83 @@
 from pygame_Chess import *
 
-class piece:
-	def __init__(piece, colour, location):
-		piece.colour = colour
-		piece.location = location
+
+class Piece:
+    def __init__(self, colour, location):
+        self.colour = colour
+        self.location = location
 
 
-class pawn(piece):
-	def __init__(piece, colour, location):
-		super().__init__(colour, location)
-		piece.img = pawn_img
-		piece.has_moved = False
+class Pawn(Piece):
+    def __init__(self, colour, location):
+        super().__init__(colour, location)
+        self.img = (white_pawn_img, black_pawn_img)
+        self.has_moved = False
 
-	def first_move():
-		piece.location[1] += 2
-		piece.has_moved = True
-	#def promote():
+    def first_move(self):
+        self.location[1] += 2
+        self.has_moved = True
 
-	def move():
-		piece.location[1] += 1
-		piece.has_moved = True
-		if (location[1] == 8):
-			piece.promote
-	def take(direction):
-		piece.location[1] += 1
-		piece.location[0] += direction
-		piece.has_moved = True
-		
-class knight(piece):
-	def __init__(piece, colour, location):
-		super().__init__(colour, location)
-		piece.img = knight_img
-		
-class bishop(piece):
-	def __init__(piece, colour, location):
-		super().__init__(colour, location)
-		piece.img = bishop_img
-		
-class rook(piece):
-	def __init__(piece, colour, location):
-		super().__init__(colour, location)
-		piece.img = rook_img
-		
-class queen(piece):
-	def __init__(piece, colour, location):
-		super().__init__(colour, location)
-		piece.img = queen_img
-		
-class king(piece):
-	def __init__(piece, colour, location):
-		super().__init__(colour, location)
-		piece.img = king_img
-		piece.has_moved = False
-	def move(direction):
-		piece.has_moved = True
-		
+    # def promote():
+
+    def move(self):
+        self.location[1] += 1
+        self.has_moved = True
+        if self.location[1] == 8:
+            self.promote
+
+    def take(self, direction):
+        Piece.location[1] += 1
+        Piece.location[0] += direction
+        Piece.has_moved = True
+
+
+class Knight(Piece):
+    def __init__(self, colour, location):
+        super().__init__(colour, location)
+        self.img = (white_knight_img, black_knight_img)
+
+
+class Bishop(Piece):
+    def __init__(self, colour, location):
+        super().__init__(colour, location)
+        self.img = (white_bishop_img, black_bishop_img)
+
+
+class Rook(Piece):
+    def __init__(self, colour, location):
+        super().__init__(colour, location)
+        self.img = (white_rook_img, black_rook_img)
+
+
+class Queen(Piece):
+    def __init__(self, colour, location):
+        super().__init__(colour, location)
+        self.img = (white_queen_img, black_queen_img)
+
+
+class King(Piece):
+    def __init__(self, colour, location):
+        super().__init__(colour, location)
+        self.img = (white_king_img, black_king_img)
+        self.has_moved = False
+
+    def move(self, direction):
+        Piece.has_moved = True
+
+
 pieces = []
+
+
 def populate():
-	c = 0
-	for c in range(2):
-		for x in range(8):
-			pieces.append(pawn(c, [x, 1 + c*5]))
-		pieces.append(rook(c, [0, c*7]))
-		pieces.append(rook(c, [7, c*7]))
-		pieces.append(bishop(c, [2, c*7]))
-		pieces.append(bishop(c, [5, c*7]))
-		pieces.append(knight(c, [1, c*7]))
-		pieces.append(knight(c, [6, c*7]))
-		pieces.append(queen(c, [3, c*7]))
-		pieces.append(king(c, [4, c*7]))
-	
-#	for x in range(8):
-#		pieces.append(pawn("black", [x, 7]))
-#	pieces.append(rook("black", [1, 8]))
-#	pieces.append(rook("black", [8, 8]))
-#	pieces.append(bishop("black", [2, 8]))
-#	pieces.append(bishop("black", [7, 8]))
-#	pieces.append(knight("black", [3, 8]))
-#	pieces.append(knight("black", [6, 8]))
-#	pieces.append(queen("black", [4, 8]))
-#	pieces.append(king("black", [5, 8]))
+    c = 0
+    for c in range(2):
+        for x in range(8):
+            pieces.append(Pawn(c, [x, 1 + c * 5]))
+        pieces.append(Rook(c, [0, c * 7]))
+        pieces.append(Rook(c, [7, c * 7]))
+        pieces.append(Bishop(c, [2, c * 7]))
+        pieces.append(Bishop(c, [5, c * 7]))
+        pieces.append(Knight(c, [1, c * 7]))
+        pieces.append(Knight(c, [6, c * 7]))
+        pieces.append(Queen(c, [3, c * 7]))
+        pieces.append(King(c, [4, c * 7]))
--- pygame_Chess.py
+++ pygame_Chess.py
@@ -4,61 +4,78 @@ import os
 import sys
 from pieces import *
 
-print(os.getcwd())
-
 
 def set_dir():
-	abspath = os.path.abspath(sys.argv[0])
-	dname = os.path.dirname(abspath)
-	os.chdir(dname)
+    abspath = os.path.abspath(sys.argv[0])
+    dname = os.path.dirname(abspath)
+    os.chdir(dname)
 
 
-screen = display.set_mode((600,600), HWSURFACE|DOUBLEBUF)
-tile_size = (int(screen.get_size()[0]/8), int((screen.get_size()[1]/8)))
+screen = display.set_mode((600, 600), HWSURFACE | DOUBLEBUF)
+tile_size = (int(screen.get_size()[0] / 8), int((screen.get_size()[1] / 8)))
 
-pawn_img = image.load("white_pawn.png")
-knight_img = image.load("white_pawn.png")
-bishop_img = image.load("white_bishop.png")
-rook_img = image.load("white_pawn.png")
-queen_img = image.load("white_pawn.png")
-king_img = image.load("white_pawn.png")
+white_pawn_img = transform.scale(image.load("white_pawn.png"), tile_size)
+white_knight_img = transform.scale(image.load("white_knight.png"), tile_size)
+white_bishop_img = transform.scale(image.load("white_bishop.png"), tile_size)
+white_rook_img = transform.scale(image.load("white_rook.png"), tile_size)
+white_queen_img = transform.scale(image.load("white_queen.png"), tile_size)
+white_king_img = transform.scale(image.load("white_king.png"), tile_size)
+black_pawn_img = transform.scale(image.load("black_pawn.png"), tile_size)
+black_knight_img = transform.scale(image.load("black_knight.png"), tile_size)
+black_bishop_img = transform.scale(image.load("black_bishop.png"), tile_size)
+black_rook_img = transform.scale(image.load("black_rook.png"), tile_size)
+black_queen_img = transform.scale(image.load("black_queen.png"), tile_size)
+black_king_img = transform.scale(image.load("black_king.png"), tile_size)
+marker_img = transform.scale(image.load("marker.png"), tile_size)
+
 
 def draw_pieces():
+    for i in pieces:
+        screen.blit(i.img[i.colour], (i.location[0] * tile_size[0], i.location[1] * tile_size[1]))
 
-	for i in pieces:
-		screen.blit(transform.scale(i.img, tile_size), (i.location[0]*tile_size[0], i.location[1]*tile_size[1]))
-		print(str(i))
+
+def right_click(position):
+    click_location = (((position[0]//tile_size[0])*tile_size[0], (position[1]//tile_size[1])*tile_size[1]))
+    screen.blit(marker_img, click_location)
+    display.flip()
+
 
 def main():
     # initialize the pygame module
-	init()
+    init()
     # load and set the logo
-	logo = image.load("Chess_logo.png")
-	display.set_icon(logo)
-	display.set_caption("Chess")
+    logo = image.load("Chess_logo.png")
+    display.set_icon(logo)
+    display.set_caption("Chess")
     # create a surface on screen
 
-	bkg = image.load("Chess_board.png")
-	screen.blit(transform.scale(bkg, screen.get_size()), (0, 0))
-	populate()
-	draw_pieces()
-	display.flip()
+    bkg = image.load("Chess_board.png")
+    screen.blit(transform.scale(bkg, screen.get_size()), (0, 0))
+    populate()
+    draw_pieces()
+    display.flip()
 
-	# define a variable to control the main loop
-	running = True
+    # define a variable to control the main loop
+    running = True
 
     # main loop
-	while running:
-		# event handling, gets all event from the event queue
-		for event in pygame.event.get():
-			# only do something if the event is of type QUIT
-			if event.type == QUIT:
-				# change the value to False, to exit the main loop
-				running = False
+    while running:
+        # event handling, gets all event from the event queue
+        for event in pygame.event.get():
+            if event.type == pygame.MOUSEBUTTONUP:
+                position = mouse.get_pos()
+                if event.button == 3:
+                    right_click(position)
+
+            if event.type == QUIT:
+                # change the value to False, to exit the main loop
+                running = False
+
+
 
 
 # run the main function only if this module is executed as the main script
 # (if you import this as a module then nothing is executed)
-if __name__=="__main__":
+if __name__ == "__main__":
     # call the main function
     main()
